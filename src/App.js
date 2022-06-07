47c9614ba33115f11add6eea73fd0a198b8af2bf
Piero Lescano <pierolescano@protonmail.com>
2022-06-07 15:41:53 -0500

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { loginUser, checkAuth } from './redux/actions/auth';
+import { useDispatch } from 'react-redux';
+import { loginUser } from './redux/actions/auth';
+import ProtectedRoutes from './components/ProtectedRoutes';
+import useAuth from './hooks/useAuth';
 
 function App() {
+  const { authChecked, loggedIn } = useAuth();
   const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth);
+
+  // TODO: I'm keeping this for testing. Delete once the login is implemented
   const handleClick = () => {
     dispatch(loginUser({ email: 'sahar@example.com', password: '123123' }));
   };
 
-  useEffect(() => {
-    dispatch(checkAuth());
-  }, []);
-
   return (
     <Router>
-      <h1>Navigation</h1>
-      <div className="App">
-        { auth.loggedIn ? 'logged in' : 'not logged in'}
-        <button type="button" onClick={handleClick}>get data</button>
+      <div>
+        { loggedIn ? 'Logged In' : <button type="button" onClick={handleClick}>Log In</button> }
       </div>
       <Routes>
-        <Route path="/" element={<h1>Houses</h1>} />
-        <Route path="/reserve" element={<h1>Reserve</h1>} />
-        <Route path="/reservations" element={<h1>Reservations</h1>} />
-        <Route path="/add_house" element={<h1>AddHouse</h1>} />
-        <Route path="/delete_house" element={<h1>DeleteHouse</h1>} />
+        <Route path="/login" element={<h1>Log In Page</h1>} />
+        <Route path="/signup" element={<h1>Sign Up Page</h1>} />
+        <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
+          <Route path="/" element={<h1>Houses Page</h1>} />
+          <Route path="/reservations" element={<h1>Reservations Page</h1>} />
+          <Route path="/add_house" element={<h1>AddHouse Page</h1>} />
+          <Route path="/delete_house" element={<h1>DeleteHouse Page</h1>} />
+        </Route>
       </Routes>
     </Router>
   );
