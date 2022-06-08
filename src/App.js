0c1ba6082425fdb8d685855946daef5942894171
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-06-09 02:50:46 +0300

    Add a side navigation menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoutes from './components/ProtectedRoutes';
+import Sidebar from './pages/Sidebar';
 import useAuth from './hooks/useAuth';
 import Login from './pages/Login';
 
@@ -7,16 +8,23 @@ function App() {
   const { authChecked, loggedIn } = useAuth();
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login loggedIn={loggedIn} />} />
-        <Route path="/signup" element={<h1>Sign Up Page</h1>} />
-        <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
-          <Route path="/" element={<h1>Houses Page</h1>} />
-          <Route path="/reservations" element={<h1>Reservations Page</h1>} />
-          <Route path="/add_house" element={<h1>AddHouse Page</h1>} />
-          <Route path="/delete_house" element={<h1>DeleteHouse Page</h1>} />
-        </Route>
-      </Routes>
+      <div className={loggedIn && 'sm:flex'}>
+        <div className={!loggedIn && 'hidden'}>
+          <Sidebar />
+        </div>
+        <div className="flex-1">
+          <Routes>
+            <Route path="/login" element={<Login loggedIn={loggedIn} />} />
+            <Route path="/signup" element={<h1>Sign Up Page</h1>} />
+            <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
+              <Route path="/" element={<h1>Houses Page</h1>} />
+              <Route path="/reservations" element={<h1>Reservations Page</h1>} />
+              <Route path="/add_house" element={<h1>AddHouse Page</h1>} />
+              <Route path="/delete_house" element={<h1>DeleteHouse Page</h1>} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
---
+++ src/pages/Sidebar.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as TiSocial from 'react-icons/ti';
+import logo from '../images/logo.png';
+
+function Sidebar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <div className="text-slate-600 h-screen absolute sm:relative inset-x-0 sm:border-r-2 ">
+
+      <button type="button" className="sm:hidden m-3 absolute" onClick={showSidebar}>
+        {!sidebar ? (
+          <svg
+            className="w-6 h-6 text-gray-500"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="4"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M4 5h16M4 12h16M4" />
+          </svg>
+        ) : (
+          <svg
+            className="w-6 h-6 text-gray-500"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="4"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        )}
+      </button>
+
+      <nav
+        className={
+        !sidebar ? 'sm:flex flex-col h-full hidden'
+          : 'flex flex-col h-full bg-white pt-7 sm:pt-0'
+      }
+      >
+        <Link className="m-4 hidden sm:flex" to="/">
+          <img src={logo} alt="logo" width={100} />
+        </Link>
+        <ul className="sm:ml-2 pt-2 pb-3 mt-6 text-center">
+          <li className="flex flex-col">
+            {[
+              ['HOUSES', '/'],
+              ['RESERVATIONS', '/reservations'],
+              ['ADD HOUSES', '/add_house'],
+              ['DELETE HOUSES', '/delete_house'],
+              ['LOGOUT', '/logout'],
+            ].map(([title, url]) => (
+              <Link
+                to={url}
+                key={title}
+                onClick={showSidebar}
+                className="text-sm font-semibold px-3 py-2 hover:bg-lime-500 hover:text-white w-full"
+              >
+                {title}
+              </Link>
+            ))}
+          </li>
+        </ul>
+
+        <div className="mt-auto text-center">
+          <ul className="flex justify-center">
+            <li>
+              <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                <TiSocial.TiSocialFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+                <TiSocial.TiSocialTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://myaccount.google.com/" target="_blank" rel="noreferrer">
+                <TiSocial.TiSocialGooglePlus />
+              </a>
+            </li>
+            <li>
+              <a href="https://vimeo.com/" target="_blank" rel="noreferrer">
+                <TiSocial.TiSocialVimeo />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.pinterest.com/" target="_blank" rel="noreferrer">
+                <TiSocial.TiSocialPinterest />
+              </a>
+            </li>
+          </ul>
+          <span className="text-xs">&copy; 2022, RENT ME</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
