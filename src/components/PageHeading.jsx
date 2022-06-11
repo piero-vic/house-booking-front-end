d50347f1e1cbeec32d5f19d2ee10ea5acf0c0f92
Piero Lescano <pierolescano@protonmail.com>
2022-06-11 17:06:24 -0500

    Add PageHeading component

---
+++ src/components/PageHeading.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const PageHeading = ({ title, subtitle }) => (
+  <div className="mb-5">
+    <h2 className="text-center text-black font-black text-3xl">{title}</h2>
+    <p className="text-center text-gray-500 font-semibold text-1xl">{subtitle}</p>
+  </div>
+);
+
+PageHeading.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
+
+export default PageHeading;
--- src/pages/reservations.js
+++ src/pages/reservations.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import DataTable from 'react-data-table-component';
 import getReservations from '../redux/actions/reservations';
+import PageHeading from '../components/PageHeading';
 
 const reservations = () => {
   const dispatch = useDispatch();
@@ -14,9 +15,9 @@ const reservations = () => {
   const { data } = useSelector((state) => state.reservations);
 
   return (
-    <div className="container w-4/5 mx-auto">
+    <div className="container w-4/5 mx-auto h-screen pt-20 sm:pt-32">
+      <PageHeading title="RESERVATIONS" subtitle="Take a look at your reservations" />
       <DataTable
-        title="Reservations"
         pagination
         columns={
           [
