93326702cbbcb9512753e3227ab28f9099a9b79b
Aime Malaika <aimemalaika1995@gmail.com>
2022-06-15 21:59:18 +0200

    Test house form rendering

--- src/__tests__/pages/newhouse.test.js
+++ src/__tests__/pages/newhouse.test.js
@@ -2,14 +2,14 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
-import Reservations from '../../pages/reservations';
+import NewHouse from '../../pages/newhouse';
 
 it('renders correctly', () => {
   const tree = renderer
     .create(
       <Provider store={store}>
         <Router>
-          <Reservations />
+          <NewHouse />
         </Router>
       </Provider>,
     )
